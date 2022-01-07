ac343afa4a4a6576dd065cc11bdc8be1f2b7a928
Christopher Mumma <christophermumma@Christophers-MacBook-Pro.local>
2022-01-07 12:23:18 -0500

    Py learning

---
+++ BasicOperatorsAndInput.py
@@ -0,0 +1,31 @@
+print ('Hello, what is your name? ')
+name = input()
+print ('Hello,', name)
+
+num1 = 45
+num2 = 3
+num3 = num1**num2
+num4 = num1//num2
+num5 = num2%num1
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num3)
+print(num4)
+print(num5)
+
+#exponents **
+#integer dvision // ex 64 / 10 = 6.4  64 // 10 = 6, does not worry about the remainder.
+# % modulous operator ex. 5 % 2 = 1, 1 is the remainder
+
+print('Pick a number: ')
+num1 = input()
+print('Pick another number: ')
+num2 = input()
+
+SUM = int(num1) + int(num2)
+print(SUM)
+print(type(num2))
---
+++ datatypes.py
@@ -0,0 +1,16 @@
+# Int
+# int 1, 4, -5, 0
+# str 'Tim', "Hello", '1' # Anything in quotes
+# bool True, False
+# float 0.32, 1.232
+
+name = 'Chris Mumma'
+print(name)
+name = 'Bob'
+print(name)
+
+age = 18
+print(age)
+
+#variable names can contain underscores and text. They can not start with a number or dashes
+# Capitals do matter in python
